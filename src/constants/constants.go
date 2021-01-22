f61f72c53d1cbe765e1d1922120f30ac45f6ba9b
Dawid Dziurla <dawidd0811@gmail.com>
2021-01-22 17:55:56 +0100

    constants: update

--- src/constants/constants.go
+++ src/constants/constants.go
@@ -2,8 +2,7 @@ package constants
 
 // TODO meson configure_file
 const (
-	AppID          = "com.github.dawidd6.checksumo"
-	LocaleDir      = "/usr/share/locale"
-	LocaleDomain   = "com.github.dawidd6.checksumo"
-	UIResourcePath = "/data/checksumo.ui"
+	AppID        = "com.github.dawidd6.checksumo"
+	LocaleDir    = "/usr/local/share/locale"
+	LocaleDomain = "com.github.dawidd6.checksumo"
 )
